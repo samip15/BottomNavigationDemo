fb517dcccc8a3d08335124c75e1b7c7d32792f53
Samip <samipgnyawali15@gmail.com>
2020-12-11 12:31:26 +0545

    Added Bottom Navigation

--- app/src/main/java/com/example/bottomnavigationdemo/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationdemo/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.bottomnavigationdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        loadFragments(new HomeFragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_dashboard:
+                fragment = new DashboardFragment();
+                break;
+            case R.id.nav_notification:
+                fragment = new NotificationFragment();
+                break;
+
+        }
+
+        return loadFragments(fragment);
+    }
+    private boolean loadFragments(Fragment fragment){
+        if (fragment==null){
+            return false;
+        }else{
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
     }
 }
